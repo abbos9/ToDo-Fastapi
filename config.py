b50808d1d5d0df8f682a73add57ed9da8a01b1ac
abbos9 <154069799+abbos9@users.noreply.github.com>
2024-07-10 18:22:01 +0500

    new

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
+from datetime import datetime
 import pytz
-
+from pydantic import BaseModel
 
 Tashkent_tz = pytz.timezone("Asia/Tashkent")
+
+class BaseModel(BaseModel):
+    create_at: datetime = datetime.now(tz=Tashkent_tz)
+    updated_at: datetime = datetime.now(tz=Tashkent_tz)
