31ccb0c6f8c2c4aaf27b1243438a6a88d408cf8d
abbos9 <154069799+abbos9@users.noreply.github.com>
2024-07-11 23:06:15 +0500

    new

--- auth.py
+++ auth.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Request, Response, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status
 from datetime import datetime, timedelta
 from typing import Annotated
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
